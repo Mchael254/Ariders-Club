fbae8704be9f888226eac4237ed46d94d0863aa3
Mchael254 <michealotieno729@gmail.com>
2025-04-09 16:51:31 +0300

    components setup

---
+++ AridersFrontend/src/app/navigation/navigation.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { faBars, faUser } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent {
+  faBars = faBars; 
+  faUser = faUser;
+
+  isSidenavOpen = false;
+
+  toggleSidenav() {
+    this.isSidenavOpen = !this.isSidenavOpen;
+  }
+
+
+
+}
