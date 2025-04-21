b1bfe343167bc5a0ea5af5ee665414acf1545e75
Mchael254 <michealotieno729@gmail.com>
2025-04-21 11:11:50 +0300

    user response

--- AridersFrontend/src/app/signin/signin.component.ts
+++ AridersFrontend/src/app/signin/signin.component.ts
@@ -64,7 +64,7 @@ export class SigninComponent {
       },
 
       error:(err)=>{
-        const errorMessage = err?.error?.message;
+        const errorMessage = err?.error?.message || 'check your internet connection';
         this.response.showError(errorMessage);
         console.error('Registration error:', err);
         this.loadingLine = false
